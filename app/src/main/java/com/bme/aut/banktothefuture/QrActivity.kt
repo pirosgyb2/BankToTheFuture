9ef302134ce5a02f107df5b72dca2c9d01aa4c81
MihalySooki <43501064+MihalySooki@users.noreply.github.com>
2020-03-28 00:18:33 +0100

    qr_reader_works

--- app/src/main/java/com/bme/aut/banktothefuture/QrActivity.kt
+++ app/src/main/java/com/bme/aut/banktothefuture/QrActivity.kt
@@ -1,50 +1,63 @@
 package com.bme.aut.banktothefuture
 
 import android.Manifest
-import android.annotation.SuppressLint
 import android.content.Intent
-import android.graphics.Bitmap
+import android.graphics.PointF
 import android.os.Bundle
 import android.provider.MediaStore
-import com.google.android.material.snackbar.Snackbar
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.dlazaro66.qrcodereaderview.QRCodeReaderView
 import com.livinglifetechway.quickpermissions.annotations.WithPermissions
 
 import kotlinx.android.synthetic.main.activity_qr.*
 
-class QrActivity : AppCompatActivity() {
+class QrActivity : AppCompatActivity(), QRCodeReaderView.OnQRCodeReadListener {
+    private val resultTextView: TextView ?= null
+    private val qrCodeReaderView:QRCodeReaderView ?= null
+
+    override fun onQRCodeRead(text:String?, points: Array<out PointF>?) {
+        resultTextView?.text = text
+        val myIntent = Intent(this, MainActivity::class.java)
+        startActivity(myIntent)
+    }
+    override fun onResume() {
+        super.onResume()
+        qrCodeReaderView?.startCamera()
+    }
+    override fun onPause() {
+        super.onPause()
+        qrCodeReaderView?.stopCamera()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_qr)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-            takePhoto()
-        }
+        getCameraPermission()
+
+        val qrCodeReaderView = findViewById<QRCodeReaderView>(R.id.qrdecoderview)
+        qrCodeReaderView.setOnQRCodeReadListener(this)
+        // Use this function to enable/disable decoding
+        qrCodeReaderView.setQRDecodingEnabled(true)
+        // Use this function to change the autofocus interval (default is 5 secs)
+        qrCodeReaderView.setAutofocusInterval(2000L)
+        // Use this function to enable/disable Torch
+        qrCodeReaderView.setTorchEnabled(true)
+        // Use this function to set front camera preview
+        qrCodeReaderView.setFrontCamera()
+        // Use this function to set back camera preview
+        qrCodeReaderView.setBackCamera()
     }
 
     @WithPermissions(
         permissions = [Manifest.permission.CAMERA]
     )
-    private fun takePhoto() {
+    private fun getCameraPermission() {
         Intent(MediaStore.ACTION_IMAGE_CAPTURE).also {
                 takePhotoIntent ->
-            takePhotoIntent.resolveActivity(packageManager)?.also {
-                startActivityForResult(takePhotoIntent, 1001)
-            }
-        }
-    }
-
-    @SuppressLint("MissingSuperCall")
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if (requestCode == 1001 && resultCode == RESULT_OK) {
-            data?.also {
-                val imageBitmap = it.extras?.get("data") as Bitmap
-                ivPhoto.setImageBitmap(imageBitmap)
-            }
+            takePhotoIntent.resolveActivity(packageManager)
         }
     }
 }
